9fe56401d8eaa1c5ec125ad95909d334e3cecf11
minori chikano <bcloudabc@gmail.com>
2023-07-03 16:41:37 +0900

    rubocopによる修正

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -21,6 +21,7 @@ class AssignsController < ApplicationController
   end
 
   private
+
   def assign_params
     params[:email]
   end
@@ -29,9 +30,9 @@ class AssignsController < ApplicationController
     if assigned_user == assign.team.owner
       I18n.t('views.messages.cannot_delete_the_leader')
     elsif Assign.where(user_id: assigned_user.id).count == 1
-      I18n.t('views.messages.cannot_delete_only_a_member') 
+      I18n.t('views.messages.cannot_delete_only_a_member')
     elsif current_user.id != assigned_user.id
-      if current_user == assign.team.owner 
+      if current_user == assign.team.owner
         assign.destroy
         set_next_team(assign, assigned_user)
         I18n.t('views.messages.delete_member')
@@ -52,14 +53,11 @@ class AssignsController < ApplicationController
     else
       I18n.t('views.messages.cannot_delete_member_4_some_reason')
     end
-
   end
 
   def email_exist?
     team = find_team(params[:team_id])
-    if team.members.exists?(email: params[:email])
-      redirect_to team_url(team), notice: I18n.t('views.messages.email_already_exists')
-    end
+    redirect_to team_url(team), notice: I18n.t('views.messages.email_already_exists') if team.members.exists?(email: params[:email])
   end
 
   def email_reliable?(address)
@@ -68,9 +66,7 @@ class AssignsController < ApplicationController
 
   def user_exist?
     team = find_team(params[:team_id])
-    unless User.exists?(email: params[:email])
-      redirect_to team_url(team), notice: I18n.t('views.messages.does_not_exist_email')
-    end
+    redirect_to team_url(team), notice: I18n.t('views.messages.does_not_exist_email') unless User.exists?(email: params[:email])
   end
 
   def set_next_team(assign, assigned_user)
@@ -78,7 +74,7 @@ class AssignsController < ApplicationController
     change_keep_team(assigned_user, another_team) if assigned_user.keep_team_id == assign.team_id
   end
 
-  def find_team(team_id)
+  def find_team(_team_id)
     Team.friendly.find(params[:team_id])
   end
 end
